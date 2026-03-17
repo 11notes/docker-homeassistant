79107ede568b796ac5e16dbf61e0c5237b09cbc8
ElevenNotes <github@11notes.ch>
2026-03-17 11:50:58 +0100

    feat: switch to go-eleven

--- build/go/entrypoint/main.go
+++ build/go/entrypoint/main.go
@@ -2,24 +2,22 @@ package main
 
 import (
 	"os"
-	"syscall"
 
-	"github.com/11notes/go/v2"
+	"github.com/11notes/go-eleven"
 )
 
 const SECRETS = "/homeassistant/etc/packages/secrets.yaml"
 
 func main() {
 	if _, err := os.Stat(SECRETS); !os.IsNotExist(err) {
-		if err := eleven.Container.EnvSubst(SECRETS); err != nil {
-			eleven.Log("WRN", "could not replace variables", err.Error())
+		if err := eleven.Container.FileContentReplaceEnv(SECRETS); err != nil {
+			eleven.Log("WRN", "could not replace variables: %s", err.Error())
 		}
 	}
+
 	if err := os.Symlink("/proc/self/fd/1", "/homeassistant/etc/home-assistant.log"); err != nil {
 		eleven.LogFatal("could not redirect log to stdout")
 	}
-	eleven.Log("INF", "starting Home Assistant")
-	if err := syscall.Exec("/usr/local/bin/python", []string{"python", "-m", "homeassistant", "--config", "/homeassistant/etc"}, os.Environ()); err != nil {
-		os.Exit(1)
-	}
+
+	eleven.Container.Run("/usr/local/bin", "python", []string{"-m", "homeassistant", "--config", "/homeassistant/etc"})
 }
